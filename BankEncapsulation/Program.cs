438094817443acae5a4e01aa1689bb4a939e6826
Eran Rotem <eranrotem123@gmail.com>
2023-02-08 13:53:22 -0600

    basic functionality

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace BankEncapsulation
 {
-    public class Program
+    public class BankAccount
     {
+        private double balance;
+        public double Balance
+        { get ; set ; }
+
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+        public void Deposit()
+        {
+            Console.WriteLine("Deposit amount: ");
+            var amount = int.Parse(Console.ReadLine());
+            balance += amount;
+
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine( $"Balance: {balance}");
+        }
+        public void Withdraw() 
+        {
+            Console.WriteLine("Withdraw amount: ");
+            var amount = int.Parse(Console.ReadLine());
+            balance -= amount;  
+        }
+        //write method to control workflow
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount Erans_Checking = new BankAccount(0);           
+            Erans_Checking.Deposit();
+            Erans_Checking.GetBalance();
+            Erans_Checking.Withdraw();
+            Erans_Checking.GetBalance();
         }
     }
 }
